be43cdccb945aad4a161cc98409edbb5b894add3
fosted1 <david.foster@ons.gov.uk>
2020-04-09 10:01:38 +0100

    Added choice storage and retrieval

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, render_template, redirect, session
+from flask import Flask, make_response, render_template, redirect, session, request
 app = Flask(__name__)
 
 choice_type = {
@@ -27,20 +27,29 @@ default_scores = {
         "testing": 0,
         }
 
-def store_choice(question_id, choice):
+
+@app.route('/store_choice', methods=["POST"])
+def store_choice():
+    question_id = request.referrer.split("/")[-1]
+    choice = request.form.get("choice")
     session[question_id] = choice
 
-def get_results():
-    return session
+    next_id = request.form.get("next_id")
+    next_id = None if next_id == "None" else next_id
+    next_url = "/results" if next_id is None else f"/question/{next_id}"
+    return redirect(next_url)
+
 
 @app.route("/")
 def index():
     session.clear()
     return make_response(render_template('index.html'), 200)
 
+
 @app.route('/index')
 def redirect_index():
     return redirect("/", code=301)
+
 
 @app.route("/question/<int:question_id>")
 def question(question_id):
@@ -50,9 +59,11 @@ def question(question_id):
         'question.html',
         question = question_data["question"],
         choices = question_data["choices"],
+        question_index = question_id,
         next_index = question_data["next_index"],
         progress = progress
         )
+
 
 @app.route("/results")
 def results():
@@ -60,6 +71,7 @@ def results():
         'results.html',
         session = session
         )
+
 
 if __name__ == "__main__":
     app.secret_key = "super_secret_key"
